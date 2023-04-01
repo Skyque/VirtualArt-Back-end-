602a646fbea71a88dd8616f140cbcfa95949be4f
ZairaF9 <98000015+ZairaF9@users.noreply.github.com>
2023-03-31 20:05:16 -0600

    Prisma Example

--- app.js
+++ app.js
@@ -14,3 +14,20 @@ app.use('/api', userRoutes);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+
+async function main() {
+    // ... you will write your Prisma Client queries here
+    const allUsers = await prisma.categorias.findMany()
+    console.log(allUsers)
+  }
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
